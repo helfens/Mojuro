e455d524e50d64a35da06616fa593087d8fd7de5
Rodolfo Helfenstein <rodolfo@000440-wl.lab.dextra.com.br>
2019-06-10 18:41:36 -0300

    Finished App Configuration

---
+++ Mojuro/Sources/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Mojuro
+//
+//  Created by Rodolfo Helfenstein on 10/06/19.
+//  Copyright © 2019 Helfens. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    return true
+  }
+
+  func applicationWillTerminate(_ application: UIApplication) { }
+
+}
---
+++ Podfile
@@ -0,0 +1,26 @@
+platform :ios, '10.0'
+
+use_frameworks!
+
+def pod_utils
+  pod 'SwiftLint'
+  pod 'R.swift'
+end
+
+target 'Mojuro' do
+
+  pod_utils
+
+end
+
+target 'MojuroTests' do
+
+  pod_utils
+
+end
+
+target 'MojuroUITests' do
+
+  pod_utils
+
+end
